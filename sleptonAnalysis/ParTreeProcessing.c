6379cd6e73c0e97205a49fea8f35b37dc4232925
graham <Graham.Wilson@cern.ch>
2020-01-10 11:45:33 -0600

    Add cutmask/bitsets and restrict to some SMS tree

--- sleptonAnalysis/ParTreeProcessing.c
+++ sleptonAnalysis/ParTreeProcessing.c
@@ -27,60 +27,61 @@ int main(int argc, char *argv[])
    //set nthreads from first input argument
    int nthreads = std::atoi (argv[1]);
 
-  //set treename to read from second input argument
-  std::string treename(argv[2]);
+   //set treename to read from second input argument
+   std::string treename(argv[2]);
 
+   //set the data tag from 3rd input argument
+   //always need a tag, if you dont want this "" should work
+   std::string tag(argv[3]);
 
-  //set the data tag from 3rd input argument
-  //always need a tag, if you dont want this "" should work
-  std::string tag(argv[3]);
+   //set outputfilename in 4th input argument
+   std::string ofilename(argv[4]);
 
-  //set outputfilename in 4th input argument
-  std::string ofilename(argv[4]);
+   //set outputfileOption in 5th argument .. either recreate or udpate
+   std::string ofileoption(argv[5]);
 
-  //set outputfileOption in 5th argument .. either recreate or udpate
-  std::string ofileoption(argv[5]);
+   //set output eventselection filename
+   std::string evtselname = "EventSelTables_"+tag+".table";
 
-  //set output eventselection filename
-  std::string evtselname = "EventSelTables_"+tag+".table";
-
-   // First enable implicit multi-threading globally, so that the implicit parallelisation is on.
-   // The parameter of the call specifies the number of threads to use.
+   //First enable implicit multi-threading globally, 
+   //so that the implicit parallelisation is on.
+   //The parameter of the call specifies the number of threads to use.
    ROOT::EnableImplicitMT(nthreads);
 
-   
-   //load up the list of files to be processed
-   //all the remaining input arguments are files to be added to the TChain
+   //load up the list of files to be processed. All the remaining 
+   //input arguments are files to be added to the TChain
    std::vector<std::string_view> ifilelist{};
-   //we need a regular string list to deal with SMS, cant get treenames without c_strings and view cant be a cstr
+   //we need a regular string list to deal with SMS, can't get treenames
+   //without c_strings and view can't be a cstr
    std::vector<std::string> ifileliststr{};
    for(int i=6; i<argc; i++){
-	ifilelist.push_back(std::string_view(argv[i]));
-	ifileliststr.push_back(std::string(argv[i]));
+       ifilelist.push_back(std::string_view(argv[i]));
+	   ifileliststr.push_back(std::string(argv[i]));
    }
 
-  //if the treename is SMS we need to store all of the tree mass points and loop over the file list for every tree
-  //
-  //
+  //if the treename is SMS we need to store all of the tree mass points 
+  //and loop over the file list for every tree
   std::vector<std::string> treenames{};
   if(treename.compare("SMS") == 0){
 	std::cout<<"Detected treename SMS"<<std::endl;
 	//loop over the masspoints and store the tree names in a vector
 	//read in file from tagname and save treenames 
 	
-	
     for(int i=0; i< ifilelist.size(); i++){
-	std::cout<<"Examining File "<<ifileliststr.at(i)<<" with nkeys = ";
+    	std::cout<<"Examining File "<<ifileliststr.at(i)<<" with nkeys = ";
     	TFile* t_file  = TFile::Open(ifileliststr.at(i).c_str());
-	int nkeys =  t_file->GetNkeys();
-	std::cout<<nkeys<<std::endl;
-	for(int j=0; j< nkeys; j++){
-		std::string trnm = std::string(t_file->GetListOfKeys()->At(j)->GetName() );
-		if (trnm.find("SMS") != std::string::npos) {
- 		   treenames.push_back( trnm );
-		}
-		
-	}
+	    int nkeys =  t_file->GetNkeys();
+	    std::cout<<nkeys<<std::endl;
+	    for(int j=0; j< nkeys; j++){
+		    std::string trnm = std::string(t_file->GetListOfKeys()->At(j)->GetName() );
+/*		    if (trnm.find("SMS") != std::string::npos) {
+ 		       treenames.push_back( trnm );
+		    } */
+// For now only look at (300, 290)files.
+		    if (trnm.find("SMS_300_290") != std::string::npos) {
+ 		       treenames.push_back( trnm );
+		    }
+	    }
     }
    // print out tree names
    // loop over tree sequence
@@ -115,10 +116,10 @@ int main(int argc, char *argv[])
 
 	h.printSelectionTables(evtSelFile);
   }
- evtSelFile.close();
+  evtSelFile.close();
 	
   return 0;	
-  }
+  }               // End of SMS if 
   else{
 	//regular BG tree, just store the KUAnalysis tree on the vector so both can be processed in a standard way
 //	treenames.push_back(treename);
--- sleptonAnalysis/histset.c
+++ sleptonAnalysis/histset.c
@@ -1,13 +1,16 @@
 #ifndef HISTS
 #define HISTS
 
-
+#include <bits/stdc++.h>  //For bitset
 #include "TH1D.h"
 #include "TH2D.h"
 #include "ROOT/TThreadedObject.hxx"
 #include "TTreeReader.h"
 #include "TTreeReaderValue.h"
 #include "myselector.C"
+
+using MyTH1D = ROOT::TThreadedObject<TH1D>;
+using MyTH2D = ROOT::TThreadedObject<TH2D>;
 
 class histset{
 	
@@ -16,19 +19,16 @@ class histset{
 	   void init(); 
 
 	   void AnalyzeEntry(myselector& s); 
-	 //  void AnalyzeEntry(mytreevalues& s);
 
-	   //bookeeping enumeration: (if we do this we dont need to worry about hist ptr copies and merging)
-//	   enum th1d_index{ind_METHist, ind_cat0NjetSHist, ind_cat1NjetSHist, numTH1Hist};
-//	   enum th2d_index{ind_cat0PtcmPtisrDphiCMIHist, ind_cat1PtcmPtisrDphiCMIHist, numTH2Hist};
-	 enum th1d_index{ind_METHist,numTH1Hist};
+	 //bookeeping enumeration: (if we do this we dont need to worry about hist ptr copies and merging)
+
+	 enum th1d_index{ind_METHist, ind_MSHist, ind_CutFlowHist, 
+                     numTH1Hist};
 	 enum th2d_index{numTH2Hist};
 	
-
 	   // make a big vector and load enumerated histograms onto the vector
-	 std::vector<ROOT::TThreadedObject<TH1D>* >  TH1Manager{};
-	 std::vector<ROOT::TThreadedObject<TH2D>* >  TH2Manager{};
-
+	 std::vector<MyTH1D*> TH1Manager{};
+	 std::vector<MyTH2D*> TH2Manager{};
 
 	  //locate the histogram and perform ptr copying 
 	  void FillTH1(int index, double x, double w);
@@ -48,14 +48,11 @@ class histset{
 	std::vector<std::string> _cutsequence{"nocut"}; //cut names read from cut list
 	std::vector<double> _npass{};//raw
 	std::vector<double> _npassw{};//weighted
-	double _weight{};
+ 	double _weight{};
 	std::vector<double> _cutval{1.}; //cut values read from cut list
 
 	//for now add absolute path of cut file
-	std::string cutlist = "/home/j342a201/slepton/ParallelSleptons/sleptonAnalysis/cutfile.list";
-
-	
-	
+	std::string cutlist = "/home/gwwilson/slepton/ParallelSleptons/sleptonAnalysis/cutfile.list";
 
 };
 
@@ -131,8 +128,9 @@ void histset::initCounts(){
 void histset::init(){
 
 //init TH1D
-	TH1Manager.at(ind_METHist) = new ROOT::TThreadedObject<TH1D>("METHist", "MET;GeV;Entries per 5 GeV bin", 140, 100, 800);
-
+	TH1Manager.at(ind_METHist) = new MyTH1D("METHist", "MET;GeV;Entries per 5 GeV bin", 140, 100.0, 800.0);
+	TH1Manager.at(ind_MSHist) = new MyTH1D("MSHist", "MS;GeV;Entries per 5 GeV bin", 50, 0.0, 250.0);
+	TH1Manager.at(ind_CutFlowHist) = new MyTH1D("CutFlowHist", "CutFlow; Cut; Weighted events", 6, -1.5, 4.5);
 }
 template <class type>
 void printvec(std::ofstream& f, std::vector<type> vec){
@@ -202,17 +200,95 @@ void histset::WriteHist(std::string outputfilename, std::string TFileOption){
 }
 void histset::AnalyzeEntry(myselector& s){
    	
-	//always make a local copy, if its a value dereference.. if you dont do this scope/dereferencing will get really weird, clunky, and unmanageable
-	//have to auto& or myreader will try to register copy of the readerarray ptr
-	auto MET = *(s.MET);
-	auto Nmu = *(s.Nmu);	
+	//Always make a local copy, if it is a value dereference. 
+    //If you don't do this, scope/dereferencing will get really weird, 
+    // clunky, and unmanageable
+	//have to auto& or myreader will try to register copy of 
+    //the readerarray ptr
+
 	auto weight = *(s.weight);
+ 	_weight = weight;
+    double w = _weight;
 
-	_weight = weight;
-	//auto& PTCM = s.PTCM;
+	//reco leptons
+	auto Nlep = *(s.Nlep);
+	auto& PT_lep = s.PT_lep;
+    auto& ID_lep = s.ID_lep;
+	auto& Eta_lep = s.Eta_lep;
+	auto& Phi_lep = s.Phi_lep;
+	auto& Charge_lep = s.Charge_lep;
+	auto& PDGID_lep = s.PDGID_lep;
+	auto& M_lep = s.M_lep;
+    auto& MiniIso_lep = s.MiniIso_lep;
 
-	FillTH1(ind_METHist, MET);
+	//susy variables for category 0 (leptons to S)
+	auto& Njet_ISR = s.Njet_ISR[0];
+	auto& Njet_S = s.Njet_S[0];
+	auto& Nlep_S = s.Nlep_S[0];
+	auto& MS = s.MS[0];
+	auto& PS = s.PS[0];
+	auto& PTISR = s.PTISR[0];
+	auto& RISR = s.RISR[0];
+	auto& MISR = s.MISR[0];
 
+	auto MET = *(s.MET);
+    auto Nbjet = *(s.Nbjet);
+    auto Nmu = *(s.Nmu);
+    auto Nele = *(s.Nele);
+    auto Is_1L = *(s.Is_1L);
+    auto Is_2L = *(s.Is_2L);
+    auto Is_3L = *(s.Is_3L);
+    auto Is_4L = *(s.Is_4L);
+
+    enum cutnames{kLeptons, kMET, kbjet, kPTISR, kRISR, NCUTS};
+// https://www.geeksforgeeks.org/c-bitset-and-its-application/
+    bitset<NCUTS> bncuts{};
+    bitset<NCUTS> bpcuts{};
+
+// First method for cut accounting (as I used to use ..)
+// Disadvantage is that we specify things using negative logic.
+    unsigned int cutmask = 0; // Events passing ALL cuts => cutmask=0
+    if( !Is_2L )        cutmask +=  1;
+    if( MET < 120.0 )   cutmask +=  2;
+    if( Nbjet > 0 )     cutmask +=  4; 
+    if( PTISR < 200.0 ) cutmask +=  8;
+    if( RISR < 0.95 )   cutmask += 16;
+
+// Second method using bitset with negative logic
+    if( !Is_2L )        bncuts[kLeptons] = 1;
+    if( MET < 120.0 )   bncuts[kMET] = 1;
+    if( Nbjet > 0 )     bncuts[kbjet] = 1; 
+    if( PTISR < 200.0 ) bncuts[kPTISR] = 1;
+    if( RISR < 0.95 )   bncuts[kRISR]= 1;
+
+// Third method using bitset with positive logic
+    if( Is_2L )         bpcuts[kLeptons] = 1;
+    if( MET > 120.0 )   bpcuts[kMET] = 1;
+    if( Nbjet = 0 )     bpcuts[kbjet] = 1; 
+    if( PTISR > 200.0 ) bpcuts[kPTISR] = 1;
+    if( RISR > 0.95 )   bpcuts[kRISR] = 1;
+// Also could use bpcuts.set(kRISR) syntax
+
+// Compare
+    cout << "cutmask: " << cutmask 
+         << " bncuts: " << bncuts 
+         << " bpcuts: " << bpcuts << endl;
+
+    if(bpcuts.all()){
+// Require all cuts are passed
+       FillTH1(ind_METHist, MET, w);
+	   FillTH1(ind_MSHist, MS, w);
+    }
+
+// Cut Flow
+    FillTH1(ind_CutFlowHist, -1.0, w);
+    for (int i=0; i<NCUTS; i++){
+       bool pass = true;
+       for (int j=0; j<=i; j++){
+          if(!bpcuts.test(j))pass = false;
+       }
+       if(pass)FillTH1(ind_CutFlowHist, i, w);
+    }
 
 
 	//event selection
@@ -224,16 +300,16 @@ void histset::AnalyzeEntry(myselector& s){
 
 		if(cut.compare("nocut")==0){
 			pass = nocut(_npass[i], 1);
-			pass = nocut(_npassw[i], weight);
+			pass = nocut(_npassw[i], w);
 		}
 		if( cut.compare("minMET")==0){
 			pass = performcut( _npass[i], 1, MET, cutvalue, std::greater<double>());
-			pass = performcut( _npassw[i], weight, MET, cutvalue, std::greater<double>());		
+			pass = performcut( _npassw[i], w, MET, cutvalue, std::greater<double>());		
                         if(!pass) break;
                 }
                 if( cut.compare("minMu")==0){
                		pass = performcut( _npass[i],1, Nmu, cutvalue, std::greater_equal<double>());
-                        pass = performcut( _npassw[i],weight,Nmu,cutvalue,std::greater_equal<double>());
+                        pass = performcut( _npassw[i], w, Nmu, cutvalue, std::greater_equal<double>());
 			if(!pass) break;
 		}
              
