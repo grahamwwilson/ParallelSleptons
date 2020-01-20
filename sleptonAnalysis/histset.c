147eb83166d999726586f4e15f446d4d409608a8
graham <Graham.Wilson@cern.ch>
2020-01-20 14:00:04 -0600

    Move all the local variable copying to mylocaltree.h

--- sleptonAnalysis/histset.c
+++ sleptonAnalysis/histset.c
@@ -309,12 +309,6 @@ void histset::WriteHist(std::string outputfilename, std::string TFileOption){
 }
 void histset::AnalyzeEntry(myselector& s){
    	
-	//Always make a local copy, if it is a value dereference. 
-    //If you don't do this, scope/dereferencing will get really weird, 
-    // clunky, and unmanageable
-	//have to auto& or myreader will try to register copy of 
-    //the readerarray ptr
-
     nseen += 1;
     if(nseen==1)cout << "Saw tag " << _tag << endl;
 
@@ -333,77 +327,15 @@ void histset::AnalyzeEntry(myselector& s){
 
     double PI =4.0*atan(1.0);
 
-	auto weight = *(s.weight);
+    #include "mylocaltree.h"
+
     double w = 137.0*weight;    //Normalize to 137 inverse fb
-
-	//reco leptons
-	auto Nlep = *(s.Nlep);
-	auto& PT_lep = s.PT_lep;
-	auto& Eta_lep = s.Eta_lep;
-	auto& Phi_lep = s.Phi_lep;
-    auto& ID_lep = s.ID_lep;            //int
-    auto& SIP3D_lep = s.SIP3D_lep;      //double 
-	auto& Charge_lep = s.Charge_lep;
-	auto& PDGID_lep = s.PDGID_lep;
-	auto& M_lep = s.M_lep;
-    auto& MiniIso_lep = s.MiniIso_lep;  //double
-
-	//susy variables for category 0 (leptons to S)
-	auto& Njet_ISR = s.Njet_ISR[0];
-	auto& Njet_S = s.Njet_S[0];
-	auto& Nlep_S = s.Nlep_S[0];
-	auto& MS0 = s.MS[0];
-	auto& PS0 = s.PS[0];
-	auto& PTISR0 = s.PTISR[0];
-	auto& RISR0 = s.RISR[0];
-	auto& MISR0 = s.MISR[0];
-
-	auto MET = *(s.MET);
-	auto MET_phi = *(s.MET_phi);
-	auto genMET = *(s.genMET);
-	auto genMET_phi = *(s.genMET_phi);
-
-    auto Njet = *(s.Njet);
-    auto Nbjet = *(s.Nbjet);
-    auto Nmu = *(s.Nmu);
-    auto Nele = *(s.Nele);
-    auto Is_1L = *(s.Is_1L);
-    auto Is_2L = *(s.Is_2L);
-    auto Is_3L = *(s.Is_3L);
-    auto Is_4L = *(s.Is_4L);
-
-//variables needed for gen level susy stuff
-    auto genNsusy = *(s.genNsusy);
-    auto& genPDGID_susy = s.genPDGID_susy;
-    auto& genMomPDGID_susy = s.genMomPDGID_susy;
-    auto& genPT_susy = s.genPT_susy;
-    auto& genEta_susy = s.genEta_susy;
-    auto& genPhi_susy = s.genPhi_susy;
-    auto& genM_susy = s.genM_susy;
-
-//gen leptons (we dont require susy)
-    auto genNlep = *(s.genNlep);
-    auto& genPDGID_lep = s.genPDGID_lep;
-    auto& genMomPDGID_lep = s.genMomPDGID_lep;
-    auto& genPT_lep = s.genPT_lep;
-    auto& genEta_lep = s.genEta_lep;
-    auto& genPhi_lep = s.genPhi_lep;
-    auto& genM_lep = s.genM_lep;
-
-//gen neutrinos
-    auto genNnu = *(s.genNnu);
-    auto& genPDGID_nu = s.genPDGID_nu;
-    auto& genMomPDGID_nu = s.genMomPDGID_nu;
-    auto& genPT_nu = s.genPT_nu;
-    auto& genEta_nu = s.genEta_nu;
-    auto& genPhi_nu = s.genPhi_nu;
+    if(genNsusy>0.0)w*=2.76;
 
     double genMET_x = genMET*cos(genMET_phi);
     double genMET_y = genMET*sin(genMET_phi);
     double MET_x = MET*cos(MET_phi);
     double MET_y = MET*sin(MET_phi);
-
-    if(genNsusy>0.0)w*=2.76;
 
     int Nposl = 0;
     int Nnegl = 0;
---
+++ sleptonAnalysis/mylocaltree.h
@@ -0,0 +1,74 @@
+    // mylocaltree.h 
+    // Here we can make local copies of everything that 
+    // we use - or even everything that exists.
+
+	//Always make a local copy, if it is a value dereference. 
+    //If you don't do this, scope/dereferencing will get really weird, 
+    // clunky, and unmanageable
+	//have to auto& or myreader will try to register copy of 
+    //the readerarray ptr
+
+	//reco leptons
+	auto Nlep = *(s.Nlep);
+	auto& PT_lep = s.PT_lep;
+	auto& Eta_lep = s.Eta_lep;
+	auto& Phi_lep = s.Phi_lep;
+    auto& ID_lep = s.ID_lep;            //int
+    auto& SIP3D_lep = s.SIP3D_lep;      //double 
+	auto& Charge_lep = s.Charge_lep;
+	auto& PDGID_lep = s.PDGID_lep;
+	auto& M_lep = s.M_lep;
+    auto& MiniIso_lep = s.MiniIso_lep;  //double
+
+	//susy variables for category 0 (leptons to S)
+	auto& Njet_ISR0 = s.Njet_ISR[0];
+	auto& Njet_S0 = s.Njet_S[0];
+	auto& Nlep_S0 = s.Nlep_S[0];
+	auto& MS0 = s.MS[0];
+	auto& PS0 = s.PS[0];
+	auto& PTISR0 = s.PTISR[0];
+	auto& RISR0 = s.RISR[0];
+	auto& MISR0 = s.MISR[0];
+
+	auto MET = *(s.MET);
+	auto MET_phi = *(s.MET_phi);
+	auto genMET = *(s.genMET);
+	auto genMET_phi = *(s.genMET_phi);
+
+    auto Njet = *(s.Njet);
+    auto Nbjet = *(s.Nbjet);
+    auto Nmu = *(s.Nmu);
+    auto Nele = *(s.Nele);
+    auto Is_1L = *(s.Is_1L);
+    auto Is_2L = *(s.Is_2L);
+    auto Is_3L = *(s.Is_3L);
+    auto Is_4L = *(s.Is_4L);
+
+//variables needed for gen level susy stuff
+    auto genNsusy = *(s.genNsusy);
+    auto& genPDGID_susy = s.genPDGID_susy;
+    auto& genMomPDGID_susy = s.genMomPDGID_susy;
+    auto& genPT_susy = s.genPT_susy;
+    auto& genEta_susy = s.genEta_susy;
+    auto& genPhi_susy = s.genPhi_susy;
+    auto& genM_susy = s.genM_susy;
+
+//gen leptons (we dont require susy)
+    auto genNlep = *(s.genNlep);
+    auto& genPDGID_lep = s.genPDGID_lep;
+    auto& genMomPDGID_lep = s.genMomPDGID_lep;
+    auto& genPT_lep = s.genPT_lep;
+    auto& genEta_lep = s.genEta_lep;
+    auto& genPhi_lep = s.genPhi_lep;
+    auto& genM_lep = s.genM_lep;
+
+//gen neutrinos
+    auto genNnu = *(s.genNnu);
+    auto& genPDGID_nu = s.genPDGID_nu;
+    auto& genMomPDGID_nu = s.genMomPDGID_nu;
+    auto& genPT_nu = s.genPT_nu;
+    auto& genEta_nu = s.genEta_nu;
+    auto& genPhi_nu = s.genPhi_nu;
+
+	auto weight = *(s.weight);
+
